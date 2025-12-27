bf1c6437cad8189d0ce176f2e9f5f4177d18f916
Ghinan Hub <136309789+Ghinan08@users.noreply.github.com>
2025-12-27 23:57:09 +0700

    Update mll.h

--- mll.h
+++ mll.h
@@ -1,11 +1,12 @@
-#ifndef MLL_H_INCLUDED
-#define MLL_H_INCLUDED
+#ifndef MLL_T_H_INCLUDED
+#define MLL_T_H_INCLUDED
 
 #include <iostream>
 using namespace std;
 
 typedef struct elmKategori *adrKategori;
 typedef struct elmProduk *adrProduk;
+typedef struct elmRelasi *adrRelasi;
 
 struct Kategori {
     string nama;
@@ -26,7 +27,12 @@ struct elmProduk {
     Produk info;
     adrProduk next;
     adrProduk prev;
-    adrKategori parent;
+};
+
+struct elmRelasi {
+    adrRelasi next;
+    adrKategori nextKategori;
+    adrProduk nextProduk;
 };
 
 struct ListKategori {
@@ -39,18 +45,26 @@ struct ListProduk {
     adrProduk last;
 };
 
+struct ListRelasi {
+    adrRelasi first;
+};
+
 void createListKategori(ListKategori &L);
 void createListProduk(ListProduk &L);
+void createListRelasi(ListRelasi &L);
 bool isEmptyKategori(ListKategori L);
-bool isEmptyProduk(adrKategori p);
 adrKategori createElmKategori(string nama);
 adrProduk createElmProduk(string nama, int harga);
+adrRelasi createElmRelasi(adrKategori K, adrProduk P);
 void insertKategori(ListKategori &L, adrKategori p);
 void insertProduk(ListProduk &L, adrProduk p);
+void insertRelasi(ListRelasi &L, adrRelasi p);
 adrKategori searchKategori(ListKategori L, string nama);
 adrProduk searchProduk(ListProduk L, string nama);
-void addRelasi(ListProduk &LP, ListKategori &LK, string namaProduk, string namaKategori);
-void showData(ListKategori LK, ListProduk LP);
-void deleteKategori(ListKategori &LK, ListProduk &LP, string namaKategori);
-void deleteProduk(ListProduk &L, string namaProduk);
-#endif // MLL_H_INCLUDED
+adrRelasi searchRelasi(ListRelasi L, adrKategori K, adrProduk P);
+void addRelasi(ListRelasi &LR, ListKategori LK, ListProduk LP, string namaProduk, string namaKategori);
+void showData(ListKategori LK, ListProduk LP, ListRelasi LR);
+void deleteKategori(ListKategori &LK, ListRelasi &LR, string namaKategori);
+void deleteProduk(ListProduk &LP, ListRelasi &LR, string namaProduk);
+
+#endif // MLL_T_H_INCLUDED
